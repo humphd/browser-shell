dc2b5c990feae44ef035d0f20909c4d0feb09077
David Humphrey <david.humphrey@senecacollege.ca>
2019-02-12 17:28:16 -0500

    Travis build

---
+++ tools/download-binaries.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Download the necessary binaries for a given release of https://github.com/humphd/browser-vm/releases/
+# Example: `download-binaries v1.0.0`
+
+mkdir -p dist/bin
+
+echo "Downloading seabios.bin"
+curl https://github.com/humphd/browser-vm/releases/download/$1/seabios.bin > dist/bin
+
+echo "Downloading vgabios.bin"
+curl https://github.com/humphd/browser-vm/releases/download/$1/vgabios.bin > dist/bin
+
+echo "Downloading v86-linux.iso"
+curl https://github.com/humphd/browser-vm/releases/download/$1/v86-linux.iso > dist/bin
